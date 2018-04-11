d65249f14f2bf673d1a74c3073653de87510ccdf
Ali Kursat Sahin <a.kursat.sahin@gmail.com>
2018-04-11 21:53:42 +0300

    TODO List düzenlendi

--- src/OyunYonetimSistemi.java
+++ src/OyunYonetimSistemi.java
@@ -2,8 +2,11 @@ import java.util.ArrayList;
 import java.util.Queue;
 
 public class OyunYonetimSistemi {
+    // TODO - ArrayList yerine ülkeler Map'de tutulacak.
     ArrayList<Ulke> ulkeler; // Map kullanılabilir, Key ile ülke adları , Value ile soru listeleri tutulur
     ArrayList<Oyuncu> oyuncular;
+    // TODO - ArrayList Soru Havuzu oluşturulacak
+    // TODO - Queue<Soru> yerine PriorityQueue<Soru> kullanılacak
     Queue<Soru> sorular;
     Oyuncu oyuncu;
 
