c3cf3d71903b75e77b76a9288123605dd822f02d
Ali Kursat Sahin <a.kursat.sahin@gmail.com>
2018-05-10 09:37:03 +0300

    Yarisma ekrani guncellendi
    Yarisma ekraninda paneller tek pencerede olacak sekilde tekrar ele alindi. bundan sonraki tasarimlarinda ayni sekilde olmasi gerekmekte. ulke secim ekrani yarisma ekraninin icine aktarilacak ve oyun dongusu tamamen burada yapilacak.

--- src/grup2/YarismaEkraniController.java
+++ src/grup2/YarismaEkraniController.java
@@ -4,21 +4,48 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import static grup2.Main.OYS;
-import static grup2.Main.yarismaEkraniPencere;
+import static java.lang.Thread.sleep;
 
 /**
  * Created by Yavuz on 26.04.2018.
  */
 public class YarismaEkraniController implements Initializable {
-    int soruSayaci = 1;
+    Integer soruSayaci = 1;
+    Soru simdikiSoru ;
+    Integer anlikPuan = 0;
+
+
+    // FXML Kullanici Paneli Objects
+    @FXML
+    public Label LabelKullaniciAdi;
 
     @FXML
+    public Label LabelKullaniciBakiyesi;
+    // End of FXML Kullanici Paneli Objects
+
+    // FXML Oyun Paneli Objects
+    @FXML
     public Label LabelUlkeAdi;
+    // End of FXML Oyun Paneli Objects
+
+    // FXML Soru Paneli Objects
+    @FXML
+    public SplitPane SplitPaneSoru;
+
+    @FXML
+    public SplitPane SplitPaneKullanici;
+
+    @FXML
+    public Label LabelZaman;
+
+    @FXML
+    public Label LabelPuan;
 
     @FXML
     public Label LabelSoruSayisi;
@@ -37,8 +64,7 @@ public class YarismaEkraniController implements Initializable {
 
     @FXML
     public Button ButtonSecenekD;
-
-
+    // End of FXML Soru Paneli Objects
 
 
 
@@ -56,12 +82,52 @@ public class YarismaEkraniController implements Initializable {
         System.out.println("ss");
         soruSayaci = 1;
         LabelUlkeAdi.setText(OYS.simdikiUlke.getUlkeAdi());
-        getNextSoru(OYS.sorular.poll());
+        simdikiSoru = getNextSoru();
+        SplitPaneSoru.setVisible(true);
+        SoruPaneliGuncelle();
     }
 
-    public void getNextSoru(Soru s){
+
+    public boolean checkIfTrue(int prediction, int actual){
+        if (prediction == actual){
+            return true;
+        }
+        return false;
+    }
+
+    public Soru getNextSoru(){
+        return OYS.sorular.poll();
+    }
+
+    public void KullaniciPaneliGuncelle(){
+        LabelKullaniciAdi.setText(OYS.oyuncu.getKullaniciAdi());
+        LabelKullaniciBakiyesi.setText(String.format("%d ____ %d",OYS.oyuncu.getBakiye(),OYS.oyuncu.getToplamPuan()));
+    }
+
+    public void SoruPaneliGuncelle(){
+        // TODO bekleme bitip panel güncellendikten sonra tuşların rengi ve aktiflik durumu düzeltilecek
+        puanGuncelle();
+        zamanGuncelle();
+        soruGuncelle(simdikiSoru);
+    }
+
+    public void puanGuncelle(){
+        LabelPuan.setText(anlikPuan.toString());
+    }
+
+    public void zamanGuncelle(){
+        LabelZaman.setText(String.format("00:0%d",soruSayaci));
+    }
+
+    public void soruGuncelle(Soru s){
         LabelSoruSayisi.setText(soruSayaci + "/5");
         LabelSoru.setText(s.soruMetni);
+        if( LabelSoru.getText().length()>60){
+            LabelSoru.setFont(Font.font(18));
+        }else {
+            LabelSoru.setFont(Font.font(23));
+        }
+
         ButtonSecenekA.setText(s.secenekA);
         ButtonSecenekB.setText(s.secenekB);
         ButtonSecenekC.setText(s.secenekC);
@@ -69,12 +135,39 @@ public class YarismaEkraniController implements Initializable {
         soruSayaci++;
     }
 
-    public void buttonAction(ActionEvent event){
+    public void buttonAction(ActionEvent actionEvent){
         if (!OYS.sorular.isEmpty()){
-            getNextSoru(OYS.sorular.poll());
+            int prediction = Integer.parseInt(((Button)actionEvent.getSource()).getId().replace("btn",""));
+
+//            switch (prediction) {
+//                case 1: break;
+//                case 2: break;
+//                case 3: break;
+//                case 4: break;
+//            }
+
+            if(checkIfTrue(prediction,simdikiSoru.dogruCevap)){
+                // TODO tuş renklendirme yapılıp 1-2 saniye beklenecek ve
+                // TODO bu bekleme sırasında tüm tuşlar disable edilecek
+                //ButtonSecenekA.setStyle("-fx-background-color: green");
+
+                anlikPuan += simdikiSoru.puanHesapla();
+            }else {
+                //ButtonSecenekA.setStyle("-fx-background-color: red");
+            }
+
+            simdikiSoru = getNextSoru();
+            SoruPaneliGuncelle();
+
         } else {
+            OYS.oyuncu.setBakiye(anlikPuan);
+            OYS.oyuncu.setToplamPuan(anlikPuan);
             OYS.writeUsersIntoJson();
-            yarismaEkraniPencere.close();
+            SplitPaneSoru.setVisible(false);
+            SplitPaneKullanici.setVisible(true);
+            KullaniciPaneliGuncelle();
+
+            //yarismaEkraniPencere.close();
         }
     }
 }
